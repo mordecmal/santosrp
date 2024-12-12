fd916ebb7632f6b9c6e1ec6485936bc5c683824a
mordecmal <mordecmal@DESKTOP-3FRJ50K.>
2024-12-12 09:18:28 +0100

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import random
+
+def get_user_choice():
+    while True:
+        user_choice = input("Enter your choice (rock,paper,scissors"): ").lower()
+        if user_choice in ["rock", ["paper"], ["scissors"]:
+            return user_choice
+        else:
+            print("eso no se vale")
+def get_computer_choice():
+    choices =  
